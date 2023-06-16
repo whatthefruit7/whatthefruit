2672b9a7a956685a83064a02d09fca4a4376d3b9
whatthefruit7 <136805437+whatthefruit7@users.noreply.github.com>
2023-06-16 16:48:23 +0530

    Initial Commit

---
+++ public/javascripts/map.js
@@ -0,0 +1,13 @@
+mapboxgl.accessToken =
+  "pk.eyJ1IjoibS0xMzVhIiwiYSI6ImNrOGsyb3ZqaDBkemkzcW10emc1eXoyNngifQ.NuSNrMKqrpdm-jxvPpx0_Q";
+const lat = 20.106960;
+const lng = 78.165940;
+
+const map = new mapboxgl.Map({
+  container: "map",
+  style: "mapbox://styles/mapbox/streets-v11",
+  zoom: 8,
+  center: [lng, lat],
+});
+
+const marker = new mapboxgl.Marker().setLngLat([lng, lat]).addTo(map);
