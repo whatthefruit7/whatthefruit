0d9395d986196352ae6ff93721d8a448962517b3
whatthefruit7 <whatthefruit.nagpur@gmail.com>
2023-06-19 22:45:30 +0530

    Revert "git testing"
    This reverts commit 1e2dc583727cad711ab8b686697c31de9d818e6a.

--- razorpay.js
+++
@@ -1,7 +0,0 @@
-var Razorpay = require('razorpay');
-const express = require('express');
-
-let instance = new Razorpay({
-    key_id : 'rzp_test_WoJC6BEFN4DCXN',
-    key_secret : 'JrzvX5PgMIsR4v7j5DJTOLT5'
-})
