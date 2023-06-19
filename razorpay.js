1e2dc583727cad711ab8b686697c31de9d818e6a
thotamohanreddy <thotamohanreddy993@gmail.com>
2023-06-19 19:31:27 +0530

    git testing

---
+++ razorpay.js
@@ -0,0 +1,7 @@
+var Razorpay = require('razorpay');
+const express = require('express');
+
+let instance = new Razorpay({
+    key_id : 'rzp_test_WoJC6BEFN4DCXN',
+    key_secret : 'JrzvX5PgMIsR4v7j5DJTOLT5'
+})
